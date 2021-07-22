b094078c05f844796cb927c764d941c81ef094be
Juan Jose Ramos Campos <eljuanjo.ramos@gmail.com>
2021-07-21 20:22:20 -0500

    [ADDED] Put method

--- mysite/persons/urls.py
+++ mysite/persons/urls.py
@@ -6,7 +6,10 @@ app_name = 'persons'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:person_id>/', views.detail, name='detail'),
-    path('new/', views.new, name='new'),
+    path('new/<int:person_id>/', views.new, name='new'),
+    path('upd/<int:person_id>/', views.upd, name='upd'),
     path('save/', views.save, name='save'),
+    path('put/<int:person_id>', views.put, name='put'),
+    
 
 ]
--- mysite/persons/views.py
+++ mysite/persons/views.py
@@ -21,18 +21,48 @@ def detail(request, person_id):
     person = get_object_or_404(Person, pk=person_id)
     return render(request, 'persons/detail.html', {'person': person})
 
-def new(request):
+def new(request, person_id):
     person = None
+    if person_id != 0:
+      person = get_object_or_404(Person, pk=person_id)
+
+
     return render(request, 'persons/form.html', {'person': person})
 
 def save(request):
     
-
-
     p = Person(name =request.POST['name'], 
     lastname=request.POST['lastname'], 
     tel=request.POST['tel'], 
     dir=request.POST['dir'],
     pub_date= date.today())
-    #p.save()
+
+    p.save()
     return render(request, 'persons/save.html', {'person' : p})
+
+
+
+
+def upd(request, person_id):
+    print(person_id)
+    person = None
+    if person_id != 0:
+      person = get_object_or_404(Person, pk=person_id)
+    return render(request, 'persons/form.html', {'person': person})
+
+def put(request, person_id):
+
+    person = get_object_or_404(Person, pk=person_id)
+
+    person.name = request.POST['name']
+    person.lastname = request.POST['lastname']
+    person.tel = request.POST['tel']
+    person.dir = request.POST['dir']
+
+    person.save()
+
+    return render(request, 'persons/save.html', {'person' : person})
+
+
+def delete(request, person_id):
+    pass
